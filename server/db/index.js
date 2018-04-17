28ab1dd0227c67cb1f798fc587ed8fa1efbce573
Molly Hanes <mollyfhanes@gmail.com>
2018-04-17 12:06:02 -0500

    required product model into db index file

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,11 @@
-const db = require('./database')
-const User = require('./user')
+const db = require('./database');
+const User = require('./user');
+const Product = require('./product');
 
 // associations go here!
 
 module.exports = {
-  db,
-  User
-}
+	db,
+	User,
+	Product
+};
