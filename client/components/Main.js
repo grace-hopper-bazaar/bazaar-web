d292afaa4dd5d988259016d85917f8b6f83f9d33
Horacio Gutierrez <hogutier@yahoo.com>
2018-04-17 15:14:18 -0500

    created AllProductsListing and SingleProductListing components working with dummy data

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Routes from './Routes'
 import Navbar from './Navbar'
+import AllProductsListing from './AllProductsListing'
 
 const Main = () => {
   return (
     <div id='main' className='fill-xy column'>
       <Navbar />
+      <AllProductsListing />
       <Routes />
     </div>
   )
